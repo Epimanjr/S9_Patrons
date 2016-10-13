869a568537616181430b2c5c4c6d3773410589b2
Antoine NOSAL <antoine.nosal@gmail.com>
2016-10-13 10:29:08 +0200

    continue les Afficheurs

--- src/main/java/fr/miage/patrons/timer/AfficheurDigitalEn.java
+++ src/main/java/fr/miage/patrons/timer/AfficheurDigitalEn.java
@@ -13,4 +13,8 @@ public class AfficheurDigitalEn extends Observeur {
         this.montre = montre;
     }
     
+    public void update() {
+        
+    }
+    
 }
--- src/main/java/fr/miage/patrons/timer/Observeur.java
+++ src/main/java/fr/miage/patrons/timer/Observeur.java
@@ -5,7 +5,7 @@ package fr.miage.patrons.timer;
  * @author Maxime BLAISE
  * @author Antoine NOSAL
  */
-public class Observeur {
+public abstract class Observeur {
     
     // no attribut
     
