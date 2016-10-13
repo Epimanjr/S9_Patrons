6832ecdcba3266f267373a1d2dbb30920b2b1cbb
Maxime BLAISE <maxime.blaise12@gmail.com>
2016-10-13 10:20:43 +0200

    Ajout Montre

--- src/main/java/fr/miage/patrons/timer/Montre.java
+++ src/main/java/fr/miage/patrons/timer/Montre.java
@@ -1,5 +1,7 @@
-
 package fr.miage.patrons.timer;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  *
@@ -7,5 +9,32 @@ package fr.miage.patrons.timer;
  * @author Antoine NOSAL
  */
 public class Montre {
-    
+
+    private Date date;
+
+    public Montre() {
+    }
+
+    public Montre(Date date) {
+        this.date = date;
+    }
+
+    /**
+     * Récupération de la date courante.
+     *
+     * @return Instance de Date
+     */
+    public Date getDate() {
+        if (this.date == null) {
+            GregorianCalendar gc = new GregorianCalendar();
+            return gc.getTime();
+        } else {
+            return this.date;
+        }
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
 }
