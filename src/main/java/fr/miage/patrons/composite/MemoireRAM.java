0d3cadd26e96e2a283ead55b618ac879c6ac0b65
Antoine Nosal <antoine.nosal@gmail.com>
2017-01-19 10:59:14 +0100

    ajoute des composants

---
+++ src/main/java/fr/miage/patrons/composite/MemoireRAM.java
@@ -0,0 +1,32 @@
+package fr.miage.patrons.composite;
+
+/**
+ *
+ * @author Antoine NOSAL
+ * @author Maxime BLAISE
+ */
+public class MemoireRAM extends Equipement {
+
+    /**
+     * Capacité de la mémoire
+     */
+    private int capacite;
+    
+    public MemoireRAM(String name, int prix, int consommation, boolean fonctionne) {
+        super(name, prix, consommation, fonctionne);
+    }
+
+    public MemoireRAM(int capacite, String name, int prix, int consommation, boolean fonctionne) {
+        super(name, prix, consommation, fonctionne);
+        this.capacite = capacite;
+    }
+    
+    public int getCapacite() {
+        return capacite;
+    }
+
+    public void setCapacite(int capacite) {
+        this.capacite = capacite;
+    }
+
+}
